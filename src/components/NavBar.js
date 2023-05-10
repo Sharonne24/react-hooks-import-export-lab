966c9606fc1507a5d17d3160ae305e476e42a24f
Sharonne24 <sharonawuor1995>
2023-05-10 19:02:54 +0300

    completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,17 @@ import React from "react";
 function NavBar() {
   return (
     <nav>
-      <a href="#home">I'm a link!</a>
+      <a href="#home">Home</a>
     </nav>
   );
 }
+
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
+
+export default NavBar;
